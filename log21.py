923b30a96320666d957be05d0ad4d180b1c00f8f
Charles <cryptocharles@gmail.com>
2016-04-06 00:04:47 +0200

    more logs

--- log21.py
+++ log21.py
@@ -1,8 +1,17 @@
 import sys
 import json
 import click
-from subprocess import check_output
+import two1
+from two1.commands.util import bitcoin_computer
 from two1.commands.config import Config
+from two1.commands import status
+from two1.server import rest_client
+from two1.server import machine_auth_wallet
+from two1.commands.util import config as two1_config
+from two1.commands.util import wallet as wallet_utils
+from two1.commands.util import account as account_utils
+from two1.commands.util import uxstring
+from two1.commands.util import exceptions
 from two1.wallet import Wallet
 from two1.bitrequests import BitTransferRequests
 
@@ -11,9 +20,50 @@ username = Config().username
 requests = BitTransferRequests(wallet, username)
 server_url = 'http://21.log21.io:9999/'
 
+def initialize_client():
+    uuid = bitcoin_computer.get_device_uuid()
+    if uuid:
+        two1.TWO1_DEVICE_ID = uuid
+    try:
+        config = two1_config.Config(two1.TWO1_CONFIG_FILE, dict())
+        ctx = dict()
+    except exceptions.FileDecodeError as e:
+        raise click.ClickException(uxstring.UxString.Error.file_decode.format((str(e))))
+    wallet = wallet_utils.get_or_create_wallet(config.wallet_path)
+    machine_auth = machine_auth_wallet.MachineAuthWallet(wallet)
+    config.username = account_utils.get_or_create_username(config, machine_auth)
+    client = rest_client.TwentyOneRestClient(two1.TWO1_HOST, machine_auth, config.username)
+    ctx['client'] = client
+    ctx['config'] = config
+    ctx['wallet'] = wallet
+    return ctx;
+
+def status_wallet(client, wallet):
+    result = status.status_wallet(client, wallet)
+    address_balances = wallet.balances_by_address(0)
+    status_addresses = []
+    for addr, balances in address_balances.items():
+        if balances['confirmed'] > 0 or balances['total'] > 0:
+            status_addresses.append(dict(address=addr, confirmed=balances['confirmed'], total=balances['total']))
+    result['addresses'] = status_addresses
+    return result
+
+def all_logs():
+    ctx = initialize_client()
+    client = ctx['client']
+    config = ctx['config']
+    wallet = ctx['wallet']
+    result = dict()
+    result['mining'] = status.status_mining(client)
+    result['wallet'] = status_wallet(client, wallet)
+    result['account'] = status.status_account(config, wallet)
+    result['logs'] = client.get_earning_logs()['logs']
+    result['earning'] = client.get_earnings()
+    return result
+
 @click.command()
 def cli():
-    json_logs = json.loads(check_output(["21", "log", "--json"]).decode("utf8"))
+    json_logs = json.loads(json.dumps(all_logs()))
     token_response = requests.get(url=server_url + 'tokens')
-    upload_response = requests.post(url=server_url + 'logs/' + token_response.text, json=json_logs)
+    upload_response = requests.post(url=server_url + 'all-logs/' + token_response.text, json=json_logs)
     click.echo(json.loads(upload_response.text))
