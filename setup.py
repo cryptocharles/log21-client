ffeb35fcb13ce903912616560dc3a2afad278ba8
cryptocharles <cryptocharles@gmail.com>
2016-06-08 08:59:36 +0200

    sensor21 support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='log21',
-    version='1',
+    version='2',
     py_modules=['log21'],
         install_requires=[
             'Click',
@@ -10,5 +10,6 @@ setup(
     entry_points='''
         [console_scripts]
         log21=log21:cli
+        log21-sensor=log21:cli_sensor
     ''',
 )
